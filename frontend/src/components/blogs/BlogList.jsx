01d89edf42242ed4359d857b43fa5cb795c12310
shrutitagade <shrutitagade507@gmail.com>
2024-09-10 18:03:08 +0530

    Updated BlogList.jsx

--- frontend/src/components/blogs/BlogList.jsx
+++ frontend/src/components/blogs/BlogList.jsx
@@ -15,23 +15,36 @@ const BlogList = () => {
     return (
         <>
             <Navbar />
-            <br></br>
+            <br />
             <div className="container mt-5">
                 <h1>All Blogs</h1>
-                {loading ? <p>Loading...</p> : error ? <p className="text-danger">{error}</p> : (
+                {/* Handle loading, error, and blogs state */}
+                {loading ? (
+                    <p>Loading...</p>
+                ) : error ? (
+                    <p className="text-danger">{error}</p>
+                ) : Array.isArray(blogs) && blogs.length > 0 ? (
                     blogs.map((blog) => (
                         <div className="card mb-3" key={blog._id}>
                             <div className="card-body">
                                 <h5 className="card-title">{blog.title}</h5>
                                 <p className="card-text">{blog.description}</p>
-                                {blog.image && <img src={blog.image} alt={blog.title} className="img-fluid" />}
+                                {blog.image && (
+                                    <img
+                                        src={blog.image}
+                                        alt={blog.title}
+                                        className="img-fluid"
+                                    />
+                                )}
                             </div>
                         </div>
                     ))
+                ) : (
+                    <p>No blogs available</p>
                 )}
             </div>
-            <br></br>
-
+            <br />
+            <Footer />
         </>
     );
 };
